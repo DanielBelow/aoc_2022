2573497009bb26d924fad4ee71ebb42e5da429ba
Daniel Below <daniel.below@tum.de>
2022-12-04 06:48:44 +0100

    [Day4] Use own range type

--- src/day04.rs
+++ src/day04.rs
@@ -1,46 +1,46 @@
 use aoc_runner_derive::{aoc, aoc_generator};
-use itertools::Itertools;
 use parse_display::{Display, FromStr};
-use std::collections::HashSet;
 
-#[derive(Display, FromStr, Debug)]
-#[display("{al}-{ah},{bl}-{bh}")]
-pub struct ParsedData {
-    al: i64,
-    ah: i64,
-    bl: i64,
-    bh: i64,
+#[derive(Display, FromStr, Copy, Clone, Debug)]
+#[display("{lo}-{hi}")]
+pub struct Range {
+    lo: i64,
+    hi: i64,
 }
 
-pub struct SectionAssignment {
-    a: HashSet<i64>,
-    b: HashSet<i64>,
+impl Range {
+    fn fully_contains(&self, other: &Self) -> bool {
+        other.lo >= self.lo && other.hi <= self.hi
+    }
+
+    fn overlaps(&self, other: &Self) -> bool {
+        self.lo <= other.hi && self.hi >= other.lo
+    }
+}
+
+#[derive(Display, FromStr, Copy, Clone, Debug)]
+#[display("{lhs},{rhs}")]
+pub struct RangePair {
+    lhs: Range,
+    rhs: Range,
 }
 
 #[aoc_generator(day4)]
-pub fn generate(inp: &str) -> Vec<SectionAssignment> {
-    inp.lines()
-        .filter_map(|it| it.parse::<ParsedData>().ok())
-        .map(|ParsedData { al, ah, bl, bh }| SectionAssignment {
-            a: (al..=ah).collect(),
-            b: (bl..=bh).collect(),
-        })
-        .collect_vec()
+pub fn generate(inp: &str) -> Vec<RangePair> {
+    inp.lines().filter_map(|it| it.parse().ok()).collect()
 }
 
 #[aoc(day4, part1)]
-pub fn part1(inp: &[SectionAssignment]) -> usize {
+pub fn part1(inp: &[RangePair]) -> usize {
     inp.iter()
-        .filter(|SectionAssignment { a, b }| a.is_subset(b) || b.is_subset(a))
+        .filter(|RangePair { lhs, rhs }| lhs.fully_contains(rhs) || rhs.fully_contains(lhs))
         .count()
 }
 
 #[aoc(day4, part2)]
-pub fn part2(inp: &[SectionAssignment]) -> usize {
+pub fn part2(inp: &[RangePair]) -> usize {
     inp.iter()
-        .filter(|SectionAssignment { a, b }| {
-            a.intersection(b).count() != 0 || b.intersection(a).count() != 0
-        })
+        .filter(|RangePair { lhs, rhs }| lhs.overlaps(rhs))
         .count()
 }
 
@@ -59,5 +59,18 @@ mod tests {
         let data = generate(inp);
         let res = part1(&data);
         assert_eq!(res, 2);
+    }
+
+    #[test]
+    fn test_sample_p2() {
+        let inp = "2-4,6-8\n\
+                          2-3,4-5\n\
+                          5-7,7-9\n\
+                          2-8,3-7\n\
+                          6-6,4-6\n\
+                          2-6,4-8";
+        let data = generate(inp);
+        let res = part2(&data);
+        assert_eq!(res, 4);
     }
 }
